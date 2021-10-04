bf8acf48e5223739c2bd505c78684549ed1a6cf7
jkain88 <jkain88@yahoo.com>
2021-10-04 21:41:42 +0800

    Clean up

--- school_management/settings/__init__.py
+++ school_management/settings/__init__.py
@@ -4,13 +4,9 @@ from .base import *
 
 PROJECT_ENVIRONMENT = os.getenv('PROJECT_ENVIRONMENT')#, 'prod')
 
-print(PROJECT_ENVIRONMENT)
-
 if PROJECT_ENVIRONMENT == 'prod':
     from .prod import *
-
 elif PROJECT_ENVIRONMENT == 'dev':
     from .dev import *
-
 else:
     from .local import *
