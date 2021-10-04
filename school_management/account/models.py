426e434dfa623bcd51ec0e1656e719700d2f92e7
Martin <martinjosh@vashcorp.com>
2021-10-04 08:57:03 +0800

    Refactor nullable fields

--- school_management/account/models.py
+++ school_management/account/models.py
@@ -1,22 +1,25 @@
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 from . import Role, AddressType
 
 
 class User(AbstractBaseUser):
-    first_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
-    role = models.CharField(max_length=20, blank=True, choices=Role.CHOICES)
+    role = models.CharField(max_length=20, choices=Role.CHOICES)
+    contact_number = PhoneNumberField(blank=True)
 
 
 class Address(models.Model):
-    street = models.CharField(max_length=100, blank=True),
-    city_area = models.CharField(max_length=100, blank=True)
-    city = models.CharField(max_length=70, blank=True)
-    province = models.CharField(max_length=70, blank=True)
-    postal_code = models.CharField(max_length=20, blank=True)
+    street = models.CharField(max_length=100, blank=True)
+    city_area = models.CharField(max_length=100)
+    city = models.CharField(max_length=70)
+    province = models.CharField(max_length=70)
+    postal_code = models.CharField(max_length=20)
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="addresses")
-    type = models.CharField(max_length=15, blank=True, choices=AddressType.CHOICES)
+    type = models.CharField(max_length=15, choices=AddressType.CHOICES)
 
