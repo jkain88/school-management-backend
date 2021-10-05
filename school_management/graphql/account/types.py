23780e4e415f764320fa344e51453690ec56fef3
jkain88 <jkain88@yahoo.com>
2021-10-05 21:38:18 +0800

    Added address type

--- school_management/graphql/account/types.py
+++ school_management/graphql/account/types.py
@@ -1,18 +1,42 @@
+import graphene
 from graphene import relay
 from graphene_django import DjangoObjectType
+import graphene_django_optimizer as gql_optimizer
 
-from ...account.models import User
+from ...account import models
+
+class Address(DjangoObjectType):
+    class Meta:
+        interfaces = [relay.Node]
+        model = models.Address
+        only_fields = [
+            "id",
+            "street",
+            "city_area",
+            "city",
+            "province",
+            "postal_code",
+            "type"
+        ]
 
 
 class User(DjangoObjectType):
+    addresses = gql_optimizer.field(
+        graphene.List(Address, description="User's list of addresses")
+    )
+
     class Meta:
         interfaces = [relay.Node]
-        model = User
-        fields = (
+        model = models.User
+        only_fields = [
             "id",
             "email",
             "first_name",
             "last_name",
             "age",
             "role"
-        )
+        ]
+
+    @staticmethod
+    def resolve_addresses(root:models.User, _):
+        return root.addresses.all()
