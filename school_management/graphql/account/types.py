0de937b67614d19f71bcc446195ba51f68bdf7a6
Martin <martinjosh@vashcorp.com>
2021-10-05 18:16:49 +0800

    Added email field on User type

--- school_management/graphql/account/types.py
+++ school_management/graphql/account/types.py
@@ -2,11 +2,13 @@ from graphene_django import DjangoObjectType
 
 from ...account.models import User
 
+
 class User(DjangoObjectType):
     class Meta:
         model = User
         fields = (
             "id",
+            "email",
             "first_name",
             "last_name",
             "age",
