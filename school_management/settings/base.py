ea30bf256a82e4ef2e5ccd31f032583af5d597cb
martin <jkain88@yahoo.com>
2021-10-20 22:34:10 +0800

    Remove AUTH_USER_MODEL

--- school_management/settings/base.py
+++ school_management/settings/base.py
@@ -77,8 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'school_management.wsgi.application'
 
-AUTH_USER_MODEL = "account.User"
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
