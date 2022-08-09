c2cb6492b436c669da3b86e2cb60d894386908d2
jkain88 <jkain88@yahoo.com>
2022-08-09 23:01:30 +0800

    Added account endpoints and UserViewSet

--- school_management/api/urls.py
+++ school_management/api/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/', include('school_management.api.urls'))
+  path('account/', include('school_management.account.urls'))
 ]
