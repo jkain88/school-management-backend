31934db8c8d3a9d7b609b2a24604052cb841da9f
jkain88 <jkain88@yahoo.com>
2022-08-11 22:24:41 +0800

    Fix set password on user create

--- school_management/account/serializers.py
+++ school_management/account/serializers.py
@@ -3,10 +3,13 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+  password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
   class Meta:
     model = User
     fields = [
       'email',
       'first_name',
-      'last_name'
+      'last_name',
+      'password'
     ]
--- school_management/account/views.py
+++ school_management/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserSerializer
@@ -8,3 +9,14 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
+
+  def create(self, request, *args, **kwargs):
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    data = serializer.validated_data
+    password = data.pop('password')
+
+    user = User.objects.create(**data)
+    user.set_password(password)
+    user.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
