0e5c10e89a69d3072d40a61970ef00a0731cd935
jkain88 <jkain88@yahoo.com>
2022-08-16 22:43:29 +0800

    Add Grade model

--- school_management/subject/models.py
+++ school_management/subject/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-from ..core import GradeLevel
+from school_management.core import GradeLevel
 
 
 class Subject(models.Model):
@@ -8,3 +9,8 @@ class Subject(models.Model):
     schedule = models.DateTimeField()
     course = models.CharField(max_length=100, blank=True)
     grade_level = models.CharField(choices=GradeLevel.CHOICES, max_length=50, blank=True)
+
+
+class Grade(TimeStampedModel):
+    subject = models.OneToOneField(Subject, on_delete=models.PROTECT)
+    value = models.DecimalField(max_digits=5, decimal_places=2)
